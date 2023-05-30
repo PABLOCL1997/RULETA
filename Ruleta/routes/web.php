cf1bc49889549daf1fac50e96d3f9d7e279be4b0
dany.zenteno <jhulian.dzt.dev@gmail.com>
2023-05-30 16:34:53 -0400

    validate router

--- Ruleta/routes/web.php
+++ Ruleta/routes/web.php
@@ -14,26 +14,30 @@ use App\Http\Controllers\UsuarioController;
 |
 */
 
+Route::fallback(function () {
+    return redirect('/'); // Cambia '/error' por la ruta a la que deseas redireccionar
+});
+
 
 Route::middleware('guest')->group(function () {
     Route::view('/', 'auth/signin')->name('/');
     Route::view('/signup', 'auth/signup')->name('signup');
     Route::view('/signin', 'auth/signin')->name('signin');
+    Route::view('/login', 'auth/signin')->name('login');
 });
 
 Route::middleware('auth')->group(function () {
     Route::view('/admin', 'layouts/admin')->name('admin');
     Route::view('/home', 'layouts/admin')->name('home');
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
-});
-
-// Auth
-Route::post('/validar-registro', [AuthController::class, 'register'])->name('validar-registro');
-Route::post('/inicia-sesion', [AuthController::class, 'login'])->name('inicia-sesion');
-Route::post('/submit-form', [UsuarioController::class, 'usuarioNuevo'])->name('submit-form');
-
-
-// Ruleta
+    
+    // Auth
+    Route::post('/validar-registro', [AuthController::class, 'register'])->name('validar-registro');
+    Route::post('/inicia-sesion', [AuthController::class, 'login'])->name('inicia-sesion');
+    Route::post('/submit-form', [UsuarioController::class, 'usuarioNuevo'])->name('submit-form');
+    
+    
+    // Ruleta
 Route::get('ruleta','RuletaController@index');
 Route::get('ruletaDatos','RuletaController@RuletaDatos');
 Route::post('ruleta/ninguno/{tnId}/{tnSumar}', 'RuletaController@HabiltarNinguno')->name('ruleta.ninguno');
@@ -45,3 +49,5 @@ Route::get('usuario','UsuarioController@index');
 Route::post('cliente_premiado/create', 'ClientePremiadoController@create')->name('cliente_premiado.create');
 Route::resource('cliente_premiado', 'ClientePremiadoController',['as' => 'cliente_premiado']);
 Route::post('cliente_premiado/anular/{tnId}', 'ClientePremiadoController@Anular')->name('cliente_premiado.Anular');
+
+});
