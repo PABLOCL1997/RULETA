12682369cbcf4b83288c593f02a98489558e0ef6
dany.zenteno <jhulian.dzt.dev@gmail.com>
2023-05-30 17:14:12 -0400

    update route

--- Ruleta/routes/web.php
+++ Ruleta/routes/web.php
@@ -24,30 +24,28 @@ Route::middleware('guest')->group(function () {
     Route::view('/signup', 'auth/signup')->name('signup');
     Route::view('/signin', 'auth/signin')->name('signin');
     Route::view('/login', 'auth/signin')->name('login');
+    // Auth
+    Route::post('/inicia-sesion', [AuthController::class, 'login'])->name('inicia-sesion');
+    Route::post('/validar-registro', [AuthController::class, 'register'])->name('validar-registro');
 });
 
 Route::middleware('auth')->group(function () {
     Route::view('/admin', 'layouts/admin')->name('admin');
     Route::view('/home', 'layouts/admin')->name('home');
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
-    
-    // Auth
-    Route::post('/validar-registro', [AuthController::class, 'register'])->name('validar-registro');
-    Route::post('/inicia-sesion', [AuthController::class, 'login'])->name('inicia-sesion');
     Route::post('/submit-form', [UsuarioController::class, 'usuarioNuevo'])->name('submit-form');
     
-    
     // Ruleta
-Route::get('ruleta','RuletaController@index');
-Route::get('ruletaDatos','RuletaController@RuletaDatos');
-Route::post('ruleta/ninguno/{tnId}/{tnSumar}', 'RuletaController@HabiltarNinguno')->name('ruleta.ninguno');
-
-// Usuario
-Route::get('usuario','UsuarioController@index');
-
-// Cliente Premiado
-Route::post('cliente_premiado/create', 'ClientePremiadoController@create')->name('cliente_premiado.create');
-Route::resource('cliente_premiado', 'ClientePremiadoController',['as' => 'cliente_premiado']);
-Route::post('cliente_premiado/anular/{tnId}', 'ClientePremiadoController@Anular')->name('cliente_premiado.Anular');
-
+    Route::get('ruleta','RuletaController@index');
+    Route::get('ruletaDatos','RuletaController@RuletaDatos');
+    Route::post('ruleta/ninguno/{tnId}/{tnSumar}', 'RuletaController@HabiltarNinguno')->name('ruleta.ninguno');
+    
+    // Usuario
+    Route::get('usuario','UsuarioController@index');
+    
+    // Cliente Premiado
+    Route::post('cliente_premiado/create', 'ClientePremiadoController@create')->name('cliente_premiado.create');
+    Route::resource('cliente_premiado', 'ClientePremiadoController',['as' => 'cliente_premiado']);
+    Route::post('cliente_premiado/anular/{tnId}', 'ClientePremiadoController@Anular')->name('cliente_premiado.Anular');
+    
 });
